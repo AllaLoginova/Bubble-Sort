8821f005497d15cc1f38f9f64c549c1e580fa2a3
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2024-11-26 22:29:33 +0500

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,20 +1,20 @@
-n = int(input())
+n = int(input())   # длина списка
 lst = list(map(int, input().split()))
 
 flag = True
-res = 0
+res = 0   # общее количество перестановок
 
 while flag:
-    count = 0
+    count = 0  # количество перестановок за 1 проход по списку
     for i in range(n-1):
         if lst[i] > lst[i+1]:
             lst[i], lst[i+1] = lst[i+1], lst[i]
             count += 1
-    if count > 0:
+    if count > 0:   # если была хотя бы 1 перестановка
         n -= 1
         res += count
     else:
-        flag = False
+        flag = False # выходим из цикла
 
-print(*lst)
-print(res)
+print(*lst) # отсортированный список
+print(res) # общее количество перестановок
